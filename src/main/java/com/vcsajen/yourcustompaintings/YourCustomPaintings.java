c6aeaa0eaf4559ee4f418ac1b8e1777b21ed5ddc
VcSaJen <VcSaJen@ya.ru>
2017-07-29 19:54:32 +0900

    Image Scaling

--- src/main/java/com/vcsajen/yourcustompaintings/YourCustomPaintings.java
+++ src/main/java/com/vcsajen/yourcustompaintings/YourCustomPaintings.java
@@ -2,6 +2,8 @@ package com.vcsajen.yourcustompaintings;
 
 import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
+import com.mortennobel.imagescaling.*;
+import com.mortennobel.imagescaling.AdvancedResizeOp;
 import com.vcsajen.yourcustompaintings.exceptions.ImageSizeLimitExceededException;
 import com.vcsajen.yourcustompaintings.util.CallableWithOneParam;
 import com.vcsajen.yourcustompaintings.util.RunnableWithOneParam;
@@ -30,11 +32,10 @@ import org.spongepowered.api.text.channel.MessageChannel;
 import org.spongepowered.api.text.format.TextColors;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.awt.image.BufferedImageOp;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -73,6 +74,10 @@ public class YourCustomPaintings {
     {
         private MessageChannel messageChannel;
         private String url;
+        private int mapsX;
+        private int mapsY;
+        private ScaleMode scaleMode;
+        private AdvancedResizeOp.UnsharpenMask unsharpenMask;
 
         public MessageChannel getMessageChannel() {
             return messageChannel;
@@ -82,12 +87,58 @@ public class YourCustomPaintings {
             return url;
         }
 
-        public UploadPaintingParams(MessageChannel messageChannel, String url) {
+        public int getMapsX() {
+            return mapsX;
+        }
+
+        public int getMapsY() {
+            return mapsY;
+        }
+
+        public ScaleMode getScaleMode() {
+            return scaleMode;
+        }
+
+        public AdvancedResizeOp.UnsharpenMask getUnsharpenMask() {
+            return unsharpenMask;
+        }
+
+        public UploadPaintingParams(MessageChannel messageChannel, String url, int mapsX, int mapsY, ScaleMode scaleMode, AdvancedResizeOp.UnsharpenMask unsharpenMask) {
             this.messageChannel = messageChannel;
             this.url = url;
+            this.mapsX = mapsX;
+            this.mapsY = mapsY;
+            this.scaleMode = scaleMode;
+            this.unsharpenMask = unsharpenMask;
         }
     }
 
+    private enum ScaleMode {
+        NoScale,
+        BSpline,
+        Bell,
+        BiCubic,
+        BiCubicHighFreqResponse,
+        BoxFilter,
+        Hermite,
+        Lanczos3,
+        Mitchell,
+        Triangle
+    }
+
+    private static void printImgInCenter(BufferedImage printOn, BufferedImage whatToPrint)
+    {
+        Graphics2D printOnImgGraphics = printOn.createGraphics();
+        try {
+            Color oldColor = printOnImgGraphics.getColor();
+            printOnImgGraphics.setPaint(new Color(255,255,255,0));
+            printOnImgGraphics.fillRect(0, 0, printOn.getWidth(), printOn.getHeight());
+            printOnImgGraphics.setColor(oldColor);
+            printOnImgGraphics.drawImage(whatToPrint, null, printOn.getWidth()/2-whatToPrint.getWidth()/2, printOn.getHeight()/2 - whatToPrint.getHeight()/2);
+        } finally {
+            printOnImgGraphics.dispose();
+        }
+    }
 
     private void runUploadPaintingTask(UploadPaintingParams params) {
         SpongeExecutorService minecraftExecutor = Sponge.getScheduler().createSyncExecutor(myPlugin);
@@ -126,7 +177,9 @@ public class YourCustomPaintings {
 
                 try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray()))
                 {
-                    img = ImageIO.read(byteArrayInputStream);
+                    BufferedImage rawImg = ImageIO.read(byteArrayInputStream);
+                    img = new BufferedImage(rawImg.getWidth(), rawImg.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+                    img.getGraphics().drawImage(rawImg, 0, 0, null);
                     params.getMessageChannel().send(Text.of("Image dimensions: "+img.getWidth()+"x"+img.getHeight()));
                 }
             }
@@ -134,15 +187,77 @@ public class YourCustomPaintings {
             ((HttpURLConnection)conn).disconnect();
             conn = null;
             params.getMessageChannel().send(Text.of("Image was downloaded successfully. Scaling…"));
-            
+            BufferedImage scaledImg = new BufferedImage(128*params.getMapsX(), 128*params.getMapsY(), BufferedImage.TYPE_4BYTE_ABGR);
+
+            double imgAspectRatio = 1.0d*img.getWidth()/img.getHeight();
+            double mapsAspectRatio = 1.0d*scaledImg.getWidth()/scaledImg.getHeight();
+
+            int scaledW = imgAspectRatio>mapsAspectRatio ? scaledImg.getWidth() : (int)Math.round(scaledImg.getHeight()*imgAspectRatio);
+            int scaledH = imgAspectRatio>mapsAspectRatio ? (int)Math.round(scaledImg.getWidth()/imgAspectRatio) : scaledImg.getHeight();
+            BufferedImage rawScaledImg = new BufferedImage(scaledW, scaledH, BufferedImage.TYPE_4BYTE_ABGR);
+            if (params.getScaleMode()!=ScaleMode.NoScale) {
+                ResampleOp resampleOp = new ResampleOp(rawScaledImg.getWidth(), rawScaledImg.getHeight());
+                resampleOp.setUnsharpenMask(params.getUnsharpenMask());
+                ResampleFilter filter = ResampleFilters.getLanczos3Filter();
+                switch (params.getScaleMode()) {
+                    case BSpline:
+                        filter = ResampleFilters.getBSplineFilter();
+                        break;
+                    case Bell:
+                        filter = ResampleFilters.getBellFilter();
+                        break;
+                    case BiCubic:
+                        filter = ResampleFilters.getBiCubicFilter();
+                        break;
+                    case BiCubicHighFreqResponse:
+                        filter = ResampleFilters.getBiCubicHighFreqResponse();
+                        break;
+                    case BoxFilter:
+                        filter = ResampleFilters.getBoxFilter();
+                        break;
+                    case Hermite:
+                        filter = ResampleFilters.getHermiteFilter();
+                        break;
+                    case Lanczos3:
+                        filter = ResampleFilters.getLanczos3Filter();
+                        break;
+                    case Mitchell:
+                        filter = ResampleFilters.getMitchellFilter();
+                        break;
+                    case Triangle:
+                        filter = ResampleFilters.getTriangleFilter();
+                        break;
+                }
+                resampleOp.setFilter(filter);
+                resampleOp.filter(img, rawScaledImg);
+                printImgInCenter(scaledImg, rawScaledImg);
+
+
+            } else {
+                printImgInCenter(scaledImg, img);
+            }
+            ImageIO.write(scaledImg, "png", new File("imagae456.png")); //TODO: Удалить
+
             params.getMessageChannel().send(Text.of("Converting to map palette…"));
 
-            List<Future<String>> futures = minecraftExecutor.invokeAll(Collections.singleton(new CallableWithOneParam<String,String>("test", s -> {
+
+
+
+
+
+
+
+            List<Future<String>> futuresGetLastMapInd = minecraftExecutor.invokeAll(Collections.singleton(new CallableWithOneParam<String,String>("test", s -> {
                 params.getMessageChannel().send(Text.of("Generating map…"));
 
                 return "returntest";
             })));
-            String s = futures.get(0).get();
+
+            List<Future<String>> futuresSetLastMapInd = minecraftExecutor.invokeAll(Collections.singleton(new CallableWithOneParam<String,String>("test", s -> {
+
+                return "returntest";
+            })));
+            String s = futuresSetLastMapInd.get(0).get();
 
 
             params.getMessageChannel().send(Text.of("Success!"));
@@ -158,7 +273,11 @@ public class YourCustomPaintings {
         } catch (ImageSizeLimitExceededException ex) {
             params.getMessageChannel().send(Text.of(TextColors.RED, ex.getMessage()));
             logger.debug("Image file size was too big while uploading painting!", ex);
-        } finally {
+        } catch (Exception ex) {
+            params.getMessageChannel().send(Text.of(TextColors.RED, "Unknown error ("+ex.getClass().getSimpleName()+") occured while uploading painting: "+ex.getMessage()));
+            throw ex;
+        }
+        finally {
             if (conn!=null)
                 ((HttpURLConnection)conn).disconnect();
         }
@@ -167,10 +286,15 @@ public class YourCustomPaintings {
 
     private CommandResult cmdMyTest(CommandSource cmdSource, CommandContext commandContext) {
         String url = commandContext.<String>getOne("URL").get();
+        int mapsX = commandContext.<Integer>getOne("MapsX").orElse(1);
+        int mapsY = commandContext.<Integer>getOne("MapsY").orElse(1);
+        ScaleMode scaleMode = commandContext.<ScaleMode>getOne("ScaleMode").get();
+        AdvancedResizeOp.UnsharpenMask unsharpenMask = commandContext.<AdvancedResizeOp.UnsharpenMask>getOne("UnsharpenMode").orElse(AdvancedResizeOp.UnsharpenMask.None);
+
         cmdSource.sendMessage(Text.of("Downloading "+url+"…"));
 
         Task task = Task.builder().execute(new RunnableWithOneParam<UploadPaintingParams>(
-                new UploadPaintingParams(cmdSource.getMessageChannel(), url), this::runUploadPaintingTask))
+                new UploadPaintingParams(cmdSource.getMessageChannel(), url, mapsX, mapsY, scaleMode, unsharpenMask), this::runUploadPaintingTask))
                 .async()
                 .submit(myPlugin);
 
@@ -206,7 +330,10 @@ public class YourCustomPaintings {
         CommandSpec uploadPaintingCmdSpec = CommandSpec.builder()
                 .description(Text.of("Upload painting from web"))
                 .extendedDescription(Text.of("Enter URL of a picture, map(s) containing painting will be generated"))
-                .arguments(GenericArguments.string(Text.of("URL")))
+                .arguments(GenericArguments.string(Text.of("URL")),
+                        GenericArguments.optional(GenericArguments.seq(GenericArguments.integer(Text.of("MapsX")), GenericArguments.integer(Text.of("MapsY")))),
+                        GenericArguments.optional(GenericArguments.enumValue(Text.of("ScaleMode"), ScaleMode.class), ScaleMode.Lanczos3)/*,
+                        GenericArguments.optional(GenericArguments.enumValue(Text.of("UnsharpenMode"), UnsharpenMask.class), UnsharpenMask.None)*/)
                 .executor(this::cmdMyTest)
                 .build();
         game.getCommandManager().register(this, uploadPaintingCmdSpec, "uploadpainting", "up-p");
